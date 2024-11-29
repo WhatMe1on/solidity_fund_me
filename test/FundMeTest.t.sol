5f0b386cb38fdb2062fe1d87c34cc70fb495506a
Whatmelon <ccacr@Device.>
2024-11-29 17:03:09 +0800

    add

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,9 +7,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    uint256 constant INPUT_FUND_AMOUNT = 0.001 ether;
+    uint256 constant START_BALANCE = 10 ether;
+    address USER_ADDRESS = makeAddr("USER");
+    uint256 tmp = 0;
 
     function setUp() external {
         fundMe = new DeployFundMe().run();
+        vm.deal(USER_ADDRESS, START_BALANCE);
     }
 
     function testUsdIsFive() public view {
@@ -17,11 +22,81 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testGetVersion() public view {
         console.log(fundMe.getVersion());
-        assert(fundMe.getVersion()>=0);
+        assert(fundMe.getVersion() >= 0);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdateFundedDataStructure()
+        public
+        M_changeTXSender(USER_ADDRESS)
+    {
+        fundMe.fund{value: INPUT_FUND_AMOUNT}();
+        uint256 storedFundAmount = fundMe.getAddressToAmountFunded(
+            USER_ADDRESS
+        );
+
+        assertEq(storedFundAmount, INPUT_FUND_AMOUNT);
+    }
+
+    function testAddFunderAndCheck()
+        public
+        M_changeTXSender(USER_ADDRESS)
+        M_funded
+    {
+        fundMe.fund{value: INPUT_FUND_AMOUNT}();
+        address funder = fundMe.getFunderAddress(0);
+        console.log(tmp);
+        assertEq(funder, USER_ADDRESS);
+    }
+
+    function testNotOwnerWithdraw()
+        public
+        M_changeTXSender(USER_ADDRESS)
+        M_funded
+    {
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithSingleFunder()
+        public
+        M_changeTXSender(USER_ADDRESS)
+        M_funded
+    {
+        uint256 startFunderBalance = address(fundMe).balance;
+        uint256 startOwnerBalance = fundMe.getOwner().balance;
+
+        vm.stopPrank();
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        uint256 endFunderBalance = address(fundMe).balance;
+        uint256 endOwnerBalance = fundMe.getOwner().balance;
+
+        assertEq(endFunderBalance, 0);
+        assertEq(startFunderBalance + startOwnerBalance, endOwnerBalance);
+    }
+
+    /*
+    Modifiers
+    */
+    modifier M_changeTXSender(address targetAddress) {
+        vm.startPrank(targetAddress);
+        _;
+        vm.stopPrank();
+    }
+
+    modifier M_funded() {
+        fundMe.fund{value: INPUT_FUND_AMOUNT}();
+        _;
     }
 }
